d94217b504f2d8a65c51ea3f943f2b246d91098b
hardik-bambhania <hardik.p.bambhania>
2017-04-20 23:24:22 +0530

    Project Structure

--- app/src/main/java/com/rh/msu/autometer/ui/activity/MainActivity.java
+++ app/src/main/java/com/rh/msu/autometer/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.rh.msu.autometer;
+package com.rh.msu.autometer.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.rh.msu.autometer.R;
 
 public class MainActivity extends AppCompatActivity {
 
