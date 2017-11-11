ac687718cd5d5725e6e4fdff6afc04c9ee8342d5
hardik-bambhania <hardik.p.bambhania>
2017-11-11 15:09:50 +0530

    Distance calculation.

--- app/src/main/java/com/rh/msu/autometer/ui/activity/MainActivity.java
+++ app/src/main/java/com/rh/msu/autometer/ui/activity/MainActivity.java
@@ -1,9 +1,15 @@
 package com.rh.msu.autometer.ui.activity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.rh.msu.autometer.R;
+import com.rh.msu.autometer.ui.fragment.AutoMapFragment;
+import com.rh.msu.autometer.ui.fragment.DriverDetailsFragment;
+import com.rh.msu.autometer.ui.fragment.MeterReadingFragment;
+import com.rh.msu.autometer.ui.fragment.TripManagerFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +17,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadMapFragment();
+        loadDriverDetailsFragment();
+        loadTripManagerFragment();
+        loadMeterReadingFragment();
+
+    }
+
+    private void loadMapFragment() {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        AutoMapFragment mapFragment = new AutoMapFragment();
+        fragmentTransaction.add(R.id.layout_map, mapFragment, "hi");
+        fragmentTransaction.commit();
+
+    }
+
+
+    private void loadDriverDetailsFragment() {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        DriverDetailsFragment driverDetailsFragment = new DriverDetailsFragment();
+        fragmentTransaction.add(R.id.layout_driver_details, driverDetailsFragment, "hi1");
+        fragmentTransaction.commit();
+    }
+
+    private void loadTripManagerFragment() {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        TripManagerFragment tripManagerFragment = new TripManagerFragment();
+        fragmentTransaction.add(R.id.layout_trip_manage, tripManagerFragment, "hi2");
+        fragmentTransaction.commit();
+    }
+
+    private void loadMeterReadingFragment() {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        MeterReadingFragment meterReadingFragment = new MeterReadingFragment();
+        fragmentTransaction.add(R.id.layout_meter_reading, meterReadingFragment, "hi3");
+        fragmentTransaction.commit();
     }
 }
