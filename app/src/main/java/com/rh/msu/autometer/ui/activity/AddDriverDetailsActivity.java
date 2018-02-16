785f46ac7f0123e3ec8e36703cae0e4066638a66
hardik-bambhania <hardik.p.bambhania>
2018-02-16 22:40:39 +0530

    driver details activity

---
+++ app/src/main/java/com/rh/msu/autometer/ui/activity/AddDriverDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.rh.msu.autometer.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.rh.msu.autometer.R;
+
+public class AddDriverDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_driver_details);
+    }
+}
