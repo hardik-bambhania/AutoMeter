cd191df9fd8b8968a3326dfa30e4062e251d5ded
hardik-bambhania <hardik.p.bambhania>
2018-03-11 15:08:51 +0530

    Check permission on splash screen

--- app/src/main/java/com/rh/msu/autometer/ui/activity/SplashActivity.java
+++ app/src/main/java/com/rh/msu/autometer/ui/activity/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.rh.msu.autometer.ui.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 
@@ -10,12 +13,35 @@ import com.rh.msu.autometer.bl.PreferenceManager;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        checkPermission();
+    }
 
+    /**
+     * Check Location permission
+     *
+     * @return
+     */
+    private void checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
+            moveForward();
+
+        } else {
+            //Ask permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    }, PERMISSION_REQUEST);
+        }
+    }
+
+    private void moveForward() {
         if (isDriverLoggedIn()) {
             Intent intent = new Intent(SplashActivity.this, MainActivity.class);
             startActivity(intent);
@@ -26,6 +52,13 @@ public class SplashActivity extends AppCompatActivity {
 
         finish();
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        checkPermission();
+    }
+
 
     private boolean isDriverLoggedIn() {
         PreferenceManager preferenceManager = PreferenceManager.getInstance(this);
